433f0dfabebb12a62c60fffdbbf0ec9760760300
Aleksandr Sapozhnikov <Sapozhnikov2012@gmail.com>
2020-04-30 23:10:10 +0300

    test push

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <header className='header'>
+                    <img src='http://freelogodesign.me/48hourslogo_data/2019/02/09/1549686787-min-mockup.jpg' width="50" height="50" alt="" />
+            
+            </header>
+
+            );
+}
+
+export default Header;
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+            <div className='content'> 
+                <div> 
+            <img id='banner' src='https://avatars.mds.yandex.net/get-zen_doc/1897860/pub_5d58f8fa8da1ce00adb2ccc1_5d58f91635c8d800af6fbf34/scale_1200' width="750" height="150" alt="" />    
+            banner
+                
+            </div> 
+                    <div> 
+                        <img id='avatar' src='https://alenkai.ru/d/ai509-269b8236501c44d492b49b57a69cb331-1.jpg' width="150" height="150" alt="" />
+                        avatar + description
+                    </div>
+                <div> My posts
+                    <div>
+                        New post
+                    </div>
+                </div>
+                <div> Posts
+                    <div> Post 1</div>
+                    <div> Post 2</div>
+                    <div> Post 3</div>
+                </div>
+            
+           
+                </div>
+
+            );
+}
+
+export default Profile;
+
